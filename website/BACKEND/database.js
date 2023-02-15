4223dad58eaec82b56e3a2a283bc618ae108c0f1
Os41 <omar41saad@outlook.com>
2023-02-15 05:03:44 +0300

    fix: solve database problem and mobile responsive

--- website/BACKEND/database.js
+++ website/BACKEND/database.js
@@ -1,18 +1,17 @@
-function addMessage(fname, lname, email, mobile, gender, date, lang, message){
-    const mysql = require("mysql2");
-    let db = mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        port: '8889',
-        database: 'nodejs'
-    });
+const mysql = require("mysql2");
+let db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    port: '3306',
+    database: 'NJSDB'
+});
 
+function addMessage(fname, lname, email, mobile, gender, date, lang, message){
     db.connect((err) => {
         if(err) throw err;
 
-        var sql = "INSERT INTO message (fname, lname, email, mobile, gender, date, lang, message) VALUES \
-        ('"+fname+"', '"+lname+"', '"+email+"', '"+mobile+"', '"+gender+"', '"+date+"', '"+lang+"', '"+message+"')";
+        var sql = `INSERT INTO message (fname, lname, email, mobile, gender, date, lang, message) VALUES ('${fname}', '${lname}', '${email}', '${mobile}', '${gender}', '${date}', '${lang}', '${message}')`;
 
         db.query(sql, (err, result) => {
             if(err) throw err;
@@ -22,19 +21,10 @@ function addMessage(fname, lname, email, mobile, gender, date, lang, message){
 }
 
 function addUser(email, password){
-    const mysql = require("mysql2");
-    let db = mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        port: '8889',
-        database: 'nodejs'
-    });
-
     db.connect((err) => {
         if(err) throw err;
 
-        var sql = "INSERT INTO user (email, password) VALUES ('"+email+"', '"+password+"')";
+        var sql = `INSERT INTO user (email, password) VALUES ('${email}', '${password}')`;
 
         db.query(sql, (err, result) => {
             if(err) throw err;
